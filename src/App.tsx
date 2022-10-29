2eca62aaec78ffdfad82b184e062a6d9cc69ea0c
farazahmad <farazahmad.email@gmail.com>
2022-10-29 13:08:06 +0530

    Form using React hook form

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Container } from "@mui/material";
+import MyForm from "./components/MyForm";
+
+function App() {
+  return (
+    <Container>
+      <MyForm />
+    </Container>
+  )
+}
+
+export default App;
